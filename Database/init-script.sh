aa2d319f01322f697ed75f6d7628335ecbc659ab
Felipe Sobral <felipesobral_@hotmail.com>
2024-04-12 16:04:59 -0300

    refactor<BT-1>: Ajustes nos Types e PKs do banco

--- Database/init-script.sh
+++ Database/init-script.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-# init-script.sh
+set -e
 
-# Aguardar o servidor PostgreSQL iniciar
-echo "Aguardando o PostgreSQL ficar pronto..."
-sleep 10
+# Caminho para o pg_hba.conf
+PG_HBA_CONF_PATH="/var/lib/postgresql/data/pg_hba.conf"
 
+# Fazendo backup do pg_hba.conf original
+cp $PG_HBA_CONF_PATH ${PG_HBA_CONF_PATH}.bak
 
-# Executar o psql ou qualquer comando SQL que você deseja
-psql -U b1nary -d tecsusdb
+# Alterando as regras de autenticação para 'md5' para conexões locais
+# shellcheck disable=SC2028
+echo "host all b1nary 0.0.0.0/0 trust
+      
+host all b1nary 127.0.0.1/32 trust
+      host all b1nary ::1/128 trust" > $PG_HBA_CONF_PATH
 
-# Manter o container rodando após a execução dos comandos
-exec "$@"
+# Adicionando as configurações padrão do pg_hba.conf ao final do arquivo
+cat ${PG_HBA_CONF_PATH}.bak >> $PG_HBA_CONF_PATH
 
-
+echo "Configurações do pg_hba.conf atualizadas."
